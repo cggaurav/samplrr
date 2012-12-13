2f0a44753afa4250283cd3e40ce13d1e5a1c9d3f
Gaurav Chandrashekar <cggaurav@gmail.com>
2012-12-14 01:24:28 +0800

    Making the UI more beautiful

--- js/samplify.js
+++ js/samplify.js
@@ -1,8 +1,3 @@
-/* Code by Andrew Mager
-*
-*  Spotify green: #74c043
-*
-**/
 window.onload = function() {
     sp = getSpotifyApi(1);
     var models = sp.require('sp://import/scripts/api/models');
@@ -12,33 +7,39 @@ window.onload = function() {
     var application = models.application;
 
 
+    //Data values
     var currentTrackSampled;
 
-    // Test minutesFromSeconds
-    // console.log(minutesFromSeconds(54));
-    // console.log(minutesFromSeconds(128));
-
-    // Handle tabs
+    // Handle tabs, do we need this?
     tabs();
-    models.application.observe(models.EVENT.ARGUMENTSCHANGED, tabs);
+    
+    //First time use
+    clearTracks();
+    updateTracks();
 
+    models.application.observe(models.EVENT.ARGUMENTSCHANGED, tabs);
 
     models.player.observe(models.EVENT.CHANGE, function(event) {
         console.log('Event change');
         console.log(event);
-        if(event.data.curtrack) {
-            $('#sampleView').empty();
-            updateTrackSample();
-        }
+        // if(event.data.curtrack) {
+        //     clearTracks();
+        //     updateTracks();
+        // }
     });
 
-    // // Handle share popup
-    // var share_element = document.getElementById('share-popup');
-    // var share_content = 'spotify:track:76a6mUM5r7VPexAj37TLjo';
-    // share_element.addEventListener('click', displayPopup);
-    // function displayPopup() {
-    //     models.application.showSharePopup(share_element, share_content);
-    // }
+
+    $("#refresh").click(function() {
+        clearTracks();
+        updateTracks();
+    });
+
+
+    function clearTracks(){
+        $("#trackSamples").empty();
+        $("#trackHeader").empty();
+        $("#artistHeaderList").empty();
+    }
 
     function tabs() {
         var args = models.application.arguments;
@@ -50,40 +51,152 @@ window.onload = function() {
         current.style.display = 'block';
     }
 
-    //Autocomplete
-    // $("#auto").autocomplete({
-    //     source: ["Alfa","Alpha","Bravo","Charlie","Delta","Echo","Foxtrot","Golf","Hotel",
-    // "India","Juliett","Juliet","Kilo","Lima","Mike","November","Oscar","Papa"]
-    // });
-
-    function updateTrackSample(){
-        console.log("We are updating!");
-        var currentTrackURI = getCurrent();
-        currentTrackSampled = currentTrackURI;
+    function updateTracks(){
+        var currentTrackURI = getCurrentTrackURI();
+        var tracksFound = "true";
         // console.log(currentTrackURI);
         $.getJSON("http://samplify.herokuapp.com/track/sampled?id=" + currentTrackURI.toString(),function(result){
-            console.log(result);
             var count = result.samples.length;
             console.log(count);
             for(var i=0; i<count;i++)
             {
-                updateTrackSampled(result.samples[i].track1,minutesFromSeconds(result.samples[i].time1),result.samples[i].artist1);
-                updateTrackSampling(result.samples[i].track2,minutesFromSeconds(result.samples[i].time2),result.samples[i].artist2);
-                updateRelationship(result.samples[i].relationship.kind,result.samples[i].relationship.partsampled);
+                tracksFound = "false";
+                updateTrackSample(result.samples[i]);
             }
         });
 
         $.getJSON("http://samplify.herokuapp.com/track/sampling?id=" + currentTrackURI.toString(),function(result){
-            console.log(result);
             var count = result.samples.length;
             console.log(count);
             for(var i=0; i<count;i++)
             {
-                updateTrackSampled(result.samples[i].track1,minutesFromSeconds(result.samples[i].time1),result.samples[i].artist1);
-                updateTrackSampling(result.samples[i].track2,minutesFromSeconds(result.samples[i].time2),result.samples[i].artist2);
-                updateRelationship(result.samples[i].relationship.kind,result.samples[i].relationship.partsampled);
+                tracksFound = "false";
+                updateTrackSample(result.samples[i]);
             }
         });
+
+        if(tracksFound.indexOf("true") != -1)
+            updateArtistHeader();
+            updateArtists();
+    }
+
+    function updateArtists(){
+
+        var artistList = getCurrentArtistList();
+        var artistFlag = "Artist Header Not Updated";
+        var j;
+        for(j=0;j< artistList.length; j++)
+        {    
+            $.getJSON("http://samplify.herokuapp.com/artist/sampled?id=" + artistList[j].uri,function(result){
+                var count = result.samples.length;
+                console.log(count);
+                for(var i=0; i<count;i++)
+                {
+                    updateArtistSample(result.samples[i], j);
+                }
+            });
+
+            $.getJSON("http://samplify.herokuapp.com/artist/sampling?id=" + artistList[j].uri,function(result){
+                var count = result.samples.length;
+                console.log(count);
+                for(var i=0; i<count;i++)
+                {
+                    updateArtistSample(result.samples[i],j);
+                }
+            });
+        }
+    }
+
+    // Updates the sample, sampling and the relationship, samples from artists
+    function updateArtistSample(sample,id){
+
+        // console.log("Getting artist samples");
+        var tag = "#artist" + (id-1).toString();
+        // console.log("Tag is " + tag);
+        var artistSamplesHTML = $(tag);
+
+        //Create Sample Context
+        var sampling_track = models.Track.fromURI(sample.track1);
+        var sampling_track_playlist = new models.Playlist();
+        sampling_track_playlist.add(sampling_track);
+        var sampling_track_player = new views.Player();
+        sampling_track_player.track = null; // Don't play the track right away
+        sampling_track_player.position = minutesFromSeconds(sample.time1);
+        sampling_track_player.context = sampling_track_playlist;
+
+        //Update Sample
+        $(sampling_track_player.node).addClass('sp-image-large');
+        var samplingDiv = $("<div></div>").addClass("sampling");
+        samplingDiv.append(sampling_track_player.node);
+
+        //Create Sample Context
+        var sampled_track = models.Track.fromURI(sample.track2);
+        var sampled_track_playlist = new models.Playlist();
+        sampled_track_playlist.add(sampled_track);
+        var sampled_track_player = new views.Player();
+        sampled_track_player.track = null; // Don't play the track right away
+        sampled_track_player.position = minutesFromSeconds(sample.time2);
+        sampled_track_player.context = sampled_track_playlist;
+
+        //Update Sample
+        $(sampled_track_player.node).addClass('sp-image-large');
+        var sampledDiv = $("<div></div>").addClass("sampled");
+        sampledDiv.append(sampled_track_player.node);
+
+
+        var relnDiv = $("<div></div>").addClass("relationship");
+        relnDiv.append(sample.relationship.partsampled + "</br>" + sample.relationship.kind);
+        // Uppppppddddddaaaaaattttteeeeee!
+        artistSamplesHTML.append(samplingDiv);
+        artistSamplesHTML.append(relnDiv);
+        artistSamplesHTML.append(sampledDiv);
+
+    }
+
+    // Update the sample, sampling and the relationship, samples of track
+    function updateTrackSample(sample){
+
+
+        updateTrackHeader();
+        var trackSamplesHTML = $("#trackSamples");
+
+        //Create Sample Context
+        var sampling_track = models.Track.fromURI(sample.track1);
+        var sampling_track_playlist = new models.Playlist();
+        sampling_track_playlist.add(sampling_track);
+        var sampling_track_player = new views.Player();
+        sampling_track_player.track = null; // Don't play the track right away
+        sampling_track_player.position = sample.time1;
+        sampling_track_player.context = sampling_track_playlist;
+
+        //Update Sample
+        $(sampling_track_player.node).addClass('sp-image-large');
+        var samplingDiv = $("<div></div>").addClass("sampling");
+        samplingDiv.append(sampling_track_player.node);
+
+        //Create Sample Context
+        var sampled_track = models.Track.fromURI(sample.track2);
+        var sampled_track_playlist = new models.Playlist();
+        sampled_track_playlist.add(sampled_track);
+        var sampled_track_player = new views.Player();
+        sampled_track_player.track = null; // Don't play the track right away
+        sampled_track_player.position = minutesFromSeconds(sample.time2);
+        sampled_track_player.context = sampled_track_playlist;
+
+        //Update Sample
+        $(sampled_track_player.node).addClass('sp-image-large');
+        var sampledDiv = $("<div></div>").addClass("sampled");
+        sampledDiv.append(sampled_track_player.node);
+
+
+        var relnDiv = $("<div></div>").addClass("relationship");
+        relnDiv.append(sample.relationship.partsampled + "</br>" + sample.relationship.kind);
+        // Uppppppddddddaaaaaattttteeeeee!
+        trackSamplesHTML.append(samplingDiv);
+        trackSamplesHTML.append(relnDiv);
+        trackSamplesHTML.append(sampledDiv);
+
+
     }
 
     function minutesFromSeconds(time)
@@ -93,58 +206,8 @@ window.onload = function() {
         return (minutes.toString() + ":" + seconds.toString());
 
     }
-    function updateRelationship(kind, partSampled){
-        
-        $("#sampleView").append('<div class="relationship">' + "<h4>" + kind + "</br>" + partSampled + "</h4>" + "</div>");
-    }
 
-    function updateTrackSampled(uri,time,artist){
-
-
-        // var single_track = models.Track.fromURI('spotify:track:0blzOIMnSXUKDsVSHpZtWL');
-        var single_track = models.Track.fromURI(uri);
-        var single_track_playlist = new models.Playlist();
-        single_track_playlist.add(single_track);
-
-        var single_track_player = new views.Player();
-        single_track_player.track = null; // Don't play the track right away
-        single_track_player.context = single_track_playlist;
-
-
-        /* Pass the player HTML code to the #single-track-player <div /> */
-        
-        $(single_track_player.node).addClass('sp-image-extra-large');
-        var div = $("<div></div>").addClass("sampled");
-        div.append(single_track_player.node);
-        var play = '<a href="' + uri + "#" + time + '"><button class="new-button">Play where Sampled</button></a>';
-        $("#sampleView").append(div);
-        // $("#sampleView").append(play);
-
-
-    }
-
-    function updateTrackSampling(uri,time,artist){
-
-        // var single_track = models.Track.fromURI('spotify:track:0blzOIMnSXUKDsVSHpZtWL');
-        var single_track = models.Track.fromURI(uri);
-        var single_track_playlist = new models.Playlist();
-        single_track_playlist.add(single_track);
-
-        var single_track_player = new views.Player();
-        single_track_player.track = null; // Don't play the track right away
-        single_track_player.context = single_track_playlist;
-
-        /* Pass the player HTML code to the #single-track-player <div /> */
-        $(single_track_player.node).addClass('sp-image-extra-large');
-        var div = $("<div></div>").addClass("sampling");
-        div.append(single_track_player.node);
-        var play = '<a href="' + uri + "#" + time + '"><button class="new-button">Play where Sampled</button></a>';
-        $("#sampleView").append(div);
-        // $("#sampleView").append(play);
-
-    }
-
-    function getCurrent(){
+    function getCurrentTrackURI(){
         // Get the track that is currently playing
         var currentTrack = player.track;
         var currentTrackURI = player.track.uri;
@@ -152,83 +215,63 @@ window.onload = function() {
         var currentAlbum = player.track.album.name;
         var currentAlbumURI = player.track.album.uri;
 
-        var consoleHTML = $("#console");
-        // if nothing currently playing
-        if (currentTrack == null) {
-            consoleHTML.html('<p>No track currently playing</p>');
-            return null;
-        } else {
-            var html = "";
-            html+= 'Now playing: ' + "<a href='" + currentTrackURI  + "'>" + currentTrack + "</a></br>";
-            html+= 'Artist: ';
-            for(var i=0; i< currentArtistList.length; i++)
-                html += '        ' + "<a href='" + currentArtistList[i].uri+ "'>" + currentArtistList[i].name +  "</a></br>";
-            html+= 'Album: ' + "<a href='" + currentAlbumURI + "'>"  + currentAlbum + "</a></br>";
-            consoleHTML.html(html);
-            return currentTrackURI;
-        }
+        return currentTrackURI;
     }
 
+    function getCurrentTrackName(){
+        // Get the track that is currently playing
+        var currentTrack = player.track;
+        var currentTrackURI = player.track.uri;
+        var currentArtistList = player.track.artists;
+        var currentAlbum = player.track.album.name;
+        var currentAlbumURI = player.track.album.uri;
 
-    function getCurrentArtistList(){
+        return currentTrack;
+    }
+
+    function getCurrentArtistListURI(){
         var currentArtistList = player.track.artists;
         var currentArtistListURI = []
         for(var i=0; i< currentArtistList.length; i++)
             currentArtistListURI.push(currentArtistList[i].uri);
-        return currentArtistListURI
+        return currentArtistListURI;
+    }
+
+    function getCurrentArtistList(){
+        return player.track.artists;
+    }
+
+    function updateTrackHeader()
+    {
+        var currentTrack = getCurrentTrackName();
+        var currentTrackURI = getCurrentTrackURI();
+        if (currentTrack == null) {
+            console.log("No track playing!");
+            return false;
+        } 
+        else {
+
+            $("#trackHeader").html("Tracks that sample or are sampled by " + "<a href='" + currentTrackURI + "'>" + currentTrack + "</a><div id='artist" + i.toString() + "'></div>")
+            return true;
+        }
+    }
+
+    function updateArtistHeader()
+    {
+        console.log("Artist Headers Updating!");
+        var artistList = getCurrentArtistList();
+        var artistHeaderList = $("#artistHeaderList");
+        for(var i=0;i<artistList.length; i++)
+        {
+            console.log(artistList[i]);
+            artistHeaderList.append('<div class="header">Tracks that sample <a href="' + artistList[i].uri + '"">' + artistList[i].name + '</a></div><div id="artist' + i.toString() + '"></div>');
+
+        }
 
     }
-    
-    // // Handle drops
-    // var drop_box = document.querySelector('#drop_box');
+    function consolify(){
 
-    // drop_box.addEventListener('dragstart', function(e){
-    //     e.dataTransfer.setData('text/html', this.innerHTML);
-    //     e.dataTransfer.effectAllowed = 'copy';
-    // }, false);
-
-    // drop_box.addEventListener('dragenter', function(e){
-    //     if (e.preventDefault) e.preventDefault();
-    //     e.dataTransfer.dropEffect = 'copy';
-    //     this.classList.add('over');
-    // }, false);
-
-    // drop_box.addEventListener('dragover', function(e){
-    //     if (e.preventDefault) e.preventDefault();
-    //     e.dataTransfer.dropEffect = 'copy';
-    //     return false;
-    // }, false);
-
-    // drop_box.addEventListener('drop', function(e){
-    //     if (e.preventDefault) e.preventDefault();
-    //     var drop = models.Playlist.fromURI(e.dataTransfer.getData('text'));
-    //     console.log(drop);
-    //     this.classList.remove('over');
-    //     var success_message = document.createElement('p');
-    //     success_message.innerHTML = 'Playlist successfully dropped: ' + drop.uri;
-    //     this.appendChild(success_message);
-    // }, false);
-
-    // // Handle multiple tracks player
-    // var library_tracks = models.library.tracks;
-
-    // var multiple_tracks_playlist = new models.Playlist();
-    // for(var i=0;i<20;i++) {
-    //     var library_track = models.Track.fromURI(library_tracks[i].data.uri);
-    //     multiple_tracks_playlist.add(library_track);
-    // }
-
-    // console.log(multiple_tracks_playlist);
-    // var multiple_tracks_player = new views.List(multiple_tracks_playlist);
-    // multiple_tracks_player.track = null; // Don't play the track right away
-    // multiple_tracks_player.context = multiple_tracks_playlist;
-   
-    // /* Pass the player HTML code to #multiple-tracks-player */
-    // var multiple_tracks_player_HTML = document.getElementById('multiple-tracks-player');
-    // multiple_tracks_player_HTML.appendChild(multiple_tracks_player.node);
-
-
-    //First time use
-    updateTrackSample();
-
+        $("#console").html("Track now playing is " + getCurrentTrackName());
+        $("#console").append("Artists now playing are " + getCurrentArtistList());
+    }
 }
